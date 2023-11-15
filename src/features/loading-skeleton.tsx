9c74a95fb14b682ddbd80047157411bfd7004be1
sfernandodhp <134980783+sfernandodhp@users.noreply.github.com>
2023-11-15 22:36:23 +1030

    Update loading-skeleton.tsx

--- src/features/loading-skeleton.tsx
+++ src/features/loading-skeleton.tsx
@@ -5,7 +5,7 @@ export const LoadingSkeleton = () => {
         width={80}
         height={80}
         alt=""
-        src="/ai-icon.png"
+        src="/logo_gday-group.png"
         className="animate-bounce"
       />
     </div>
